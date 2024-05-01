cfcf8e47e10cebf2d4aed06c281f79ea677dd87f
Ícaro Rafael <icarorafa@gmail.com>
2024-05-01 20:42:32 +0100

    feature: Set up Handlers, Created Mappings, POST route without saving to DB

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Add Services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure HTTP request PipeLine
+
+app.MapCarter();
 
 app.Run();
